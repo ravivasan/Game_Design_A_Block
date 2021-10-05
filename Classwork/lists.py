940ad051b830965b183fe536a2885666eff3d86e
Ravi Vasan <vasanr24@greenhill.org>
2021-10-05 09:07:51 -0500

    quiz 1

--- Classwork/lists.py
+++ Classwork/lists.py
@@ -2,46 +2,85 @@
 #learning about collections in python
 #list
 
-myfruits = ["apple," "berries", "bananas"]
-
-# for x in myfruits:
-#     print (x)
-# for x in myfruits[0]:
-#     print ("_", end = " ")
-
 #homework
 #word guessing game but you ask for one letter at a time
 
 import os, sys, random
 os.system ('cls')
 
+def updateWord(fruits, guesses):
+    for letter in fruits:
+        if letter in guesses:
+            print(letter, end = " ")
+        else:
+            print('_', end = ' ')
+
+#define fuction for Menu
+def Menu():
+    print("##########################################################")
+    print("#                          Menu                          #")
+    print("#                      1) animals                        #")
+    print("#                      2) fruits                         #")
+    print("#                      3) colors                         #")
+    print("#                      4) exit                           #")
+    print("# to play the game, select 1-3. to exit the game selet 4.#")
+    print("##########################################################")
+    sel=input("what would you like to do?")
+    # try:
+    # except:
+    sel=int(sel)
+    return sel
+def selWord(sel):
+    if sel ==1:
+        word = random.choice(animals)
+    elif sel ==2:
+        word = random.choice(fruits)
+    else:
+        word = random.choice(colors)
+    return word
+colors = ('red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet')
+animals = ('lion', 'tiger', 'dog', 'cat', 'monkey', 'giraffe', 'penguin')
 fruits = ('apple', 'banana', 'strawberry', 'blueberry', 'grape', 'watermelon', 'orange')
 
 print ("welcome")
-print ("guess a letter to see if it is in the fruit. you have 10 tries.")
+print ("guess a letter to see if it is in the word. you have 10 tries.")
+name = input("what is your name?")
+counter = 0
+sel = Menu()
+answer=input(name + ", do you want to play the game?")
+while sel != 4:
+    print
+    print ("good luck!")
 
-answer=input("do you want to play a game?")
-while ('y' in answer):
-    guesses = 0
+while ('y' in answer):#control the number of games played
+    turns = 10
+    counter += 1
+
+    guesses = ' '
+
     myword=random.choice(fruits)
-    #print (myword)
-    for x in fruits:
-        print (x)
-    for x in fruits[0]:
-        print ("_", end = " ")
-    while (guesses < 10 and myfruits != myword):
-        # for char in myword:
-        #     print (char)
+    myword = myword.lower()
 
-        char = input("give me a letter")
-        if myword == (input):
-            print ("yes!! you win!")
+    mywordCount = len(myword)
+    letCount= 0
+
+    print(myword)
+    updateWord(fruits, guesses)
+
+    while turns > 0 and letCount > mywordCount: 
+        print()
+        newguess = input ("give me a letter, " + name)
+        newguess = newguess.lower()
+        if newguess in myword:
+                guesses += newguess
+            letCount +=1
+            print ("you guessed one letter")
         else:
-            print("guess another!")
-        guesses += 1
-        if char == myword:
-            print ("you win!!")
-        else:
-            print ("guess again!")
-        
-    answer=input("do you want to play again?")
+            turns -= 1
+            print ("sorry, thats wrong. you have ", turns, " turns left")
+        updateWord(fruits, guesses)
+
+    answer = input(name + ", do you want to play again?")
+    os.system('cls')
+    sel = Menu()
+ 
---
+++ Classwork/quiz1.py
@@ -0,0 +1,35 @@
+#Ravi Vasan
+#this is my quiz
+
+
+# print(len("Hello World"))
+
+strString = "Study.com is a wonderful resource."
+
+
+#questions #1-#5
+# print(strString[15:24].find("der")+len(strString))
+
+# print(strString.find("great"))
+# print(strString.find("wonderful"))
+# print(strString.find("."))
+
+#question #6
+# var1=17
+# var2=4
+# result = var1 % var2
+
+# #question #7
+# print("I am so...\t tired")
+
+# #question #8
+# print("I need sleep so badly \n I should've gotten coffee this morning")
+
+# #question #9
+# name = input("what is your name")
+# print("have a great day " + name )
+
+#question #10
+Mylist = ['0', '1', '2', '3', '4', '6', '7', '8', '9','10']
+Mylist[5] = '5'
+print(Mylist)
